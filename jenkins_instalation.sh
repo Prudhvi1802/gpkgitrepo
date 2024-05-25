dbc3b3f1f361dcc01e17a7d94057d076c35a51c8
Prudhvi Chowdary <160645412+Prudhvi1802@users.noreply.github.com>
2024-05-25 22:35:41 +0530

    Update jenkins_instalation.sh

--- jenkins_instalation.sh
+++ jenkins_instalation.sh
@@ -28,3 +28,26 @@ sudo systemctl enable jenkins
 # Output the initial admin password
 #echo "Jenkins installation completed. The initial admin password is:i
 #sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+-----------------------------------------------------------------
+#for amazon linux2 
+sudo yum update -y
+# Jenkins repo is added to yum.repos.d
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import key from Jenkins
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Install Java-11
+amazon-linux-extras install java-openjdk11 -y
+
+# Install Jenkins
+yum install jenkins -y
+
+# You can enable the Jenkins service to start at boot with the command:
+systemctl enable jenkins
+
+# You can start the Jenkins service with the command:
+systemctl start jenkins
+
+# You can check the status of the Jenkins service using the command:
+systemctl status jenkins
